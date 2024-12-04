03fb0a74ed2264fe6bc31a7143875aadbc4c7aa6
Xyrill Dereck Cañete <xdcanet@gmail.com>
2024-12-05 01:44:09 +0800

    Customer Feedback Implementation

--- BCD/settings.py
+++ BCD/settings.py
@@ -53,6 +53,7 @@ INSTALLED_APPS = [
     'home',
     'booking',
     'inventory',
+    'feedback',
     'common'
 ]
 
--- BCD/urls.py
+++ BCD/urls.py
@@ -28,6 +28,9 @@ urlpatterns = [
     path('home/', include('home.urls')),
     path('booking/', include('booking.urls')),
     path('inventory/', include('inventory.urls')),
+    path('feedback/', include('feedback.urls')),
+
+
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
