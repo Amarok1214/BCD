d3bb64cbc6dac255400ccf97d5eed0c91369961c
KArruuuu <169058702+KArruuuu@users.noreply.github.com>
2024-12-05 03:29:34 +0800

    the polished admin

--- booking/models.py
+++ booking/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 from inventory.models import Car
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 
 class Reservation(models.Model):
     # Link to the rented car
@@ -24,3 +26,12 @@ class Reservation(models.Model):
 
     def __str__(self):
         return f"Reservation for {self.car} by {self.user.get_full_name()} ({self.booking_status})"
+
+@receiver(post_save, sender=Reservation)
+def update_car_availability(sender, instance, **kwargs):
+    if instance.booking_status == 'Completed':
+        instance.car.availability = "Available"
+        instance.car.save()
+    elif instance.booking_status in ['Pending', 'Confirmed', 'Canceled']:
+        instance.car.availability = "Rented"
+        instance.car.save()
